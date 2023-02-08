eda333f22aba31e859ad5d544c287456fad16eab
Sarthak30-03-2003 <sarthak8544@gmail.com>
2023-02-08 17:40:21 +0530

    day-2

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,8 @@
+let continent="Asia"
+let country="India"
+let population=1000000000;
+console.log("Continent::"+continent)
+console.log("Country::"+country)
+console.log("Population::"+population)
+console.log("Population::"+population)
+console.log("Population::"+population)
