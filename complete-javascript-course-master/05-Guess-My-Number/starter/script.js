1c6252ad93303e17c8005719e6b82e79ba5b8e97
Sarthak30-03-2003 <sarthak8544@gmail.com>
2023-02-13 00:42:39 +0530

    dom

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,1 +1,71 @@
 'use strict';
+
+const checkBtn = document.querySelector('.check')
+const againBtn = document.querySelector('.again')
+
+//Creating Random Number
+let randomNumber = Math.trunc(Math.random() * 20) + 1
+
+
+//SCORE
+let score = 20;
+let highScore = 0;
+
+//FUNCTION TO IMPLEMENT LOGIC
+function logic() {
+
+    //Storing Guess
+    const guess = Number(document.querySelector(".guess").value)
+    console.log(guess, randomNumber)
+
+    //CHECKING 
+    if (score > 1) {
+        if (!guess) {
+            document.querySelector('.message').textContent = "🤷‍♂️ No Number!";
+        }
+        else if (guess === randomNumber) {
+            document.querySelector('.message').textContent = "🎊 Correct Number!";
+            handleHighScore();
+            document.querySelector('body').style.backgroundColor = 'green'
+            document.querySelector('.number').textContent = randomNumber
+        }
+        else if (guess < randomNumber) {
+            document.querySelector('.message').textContent = "📉 Too Low!";
+            score--;
+            document.querySelector('.score').textContent = score;
+        }
+        else if (guess > randomNumber) {
+            document.querySelector('.message').textContent = "📈 Too High!";
+            score--;
+            document.querySelector('.score').textContent = score;
+        }
+    } else {
+        score = 0;
+        document.querySelector('.score').textContent = score;
+        document.querySelector('.message').textContent = "💥You Loose!💥";
+        document.querySelector('.check').style.cursor = 'not-allowed';
+        document.querySelector('body').style.backgroundColor = 'red'
+
+    }
+}
+
+function reset() {
+    randomNumber = Math.trunc(Math.random() * 20) + 1
+    score = 20;
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?'
+    document.querySelector('.guess').textContent = ''
+    console.log(randomNumber)
+    document.querySelector('body').style.backgroundColor = '#222'
+}
+
+function handleHighScore() {
+    if (highScore <= score) {
+        document.querySelector('.highscore').textContent = score;
+        highScore = score;
+    }
+}
+
+checkBtn.addEventListener('click', logic)
+againBtn.addEventListener('click', reset)
+console.log(randomNumber)
--- complete-javascript-course-master/06-Modal/starter/script.js
+++ complete-javascript-course-master/06-Modal/starter/script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const openModalBtn = document.querySelectorAll('.show-modal')
+const closeModalBtn = document.querySelector('.close-modal')
+const overlay = document.querySelector('.overlay')
+const modal = document.querySelector('.modal')
+
+console.log(openModalBtn)
+
+const handleClick = () => {
+    modal.classList.toggle('hidden')
+    overlay.classList.toggle('hidden')
+}
+
+for (let i = 0; i < openModalBtn.length; i++)
+    openModalBtn[i].addEventListener('click', handleClick)
+
+
+closeModalBtn.addEventListener('click', handleClick)
+overlay.addEventListener('click', handleClick)
+
+document.addEventListener('keydown', (e) => {
+    console.log(e.key)
+    if (e.key === 'Escape') handleClick()
+})
