e86ca6c84e23077c5064087a051af4dbf3f77f0c
Sarthak30-03-2003 <sarthak8544@gmail.com>
2023-02-26 16:58:29 +0530

    timeIntervals

--- complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -43,7 +43,7 @@ const account2 = {
     '2020-02-05T16:33:06.386Z',
     '2020-04-10T14:43:26.374Z',
     '2020-06-25T18:49:59.371Z',
-    '2020-07-26T12:01:20.894Z',
+    '2023-02-26T12:01:20.894Z',
   ],
   currency: 'USD',
   locale: 'en-US',
@@ -81,18 +81,38 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // Functions
 
-const displayMovements = function (movements, sort = false) {
+const formatMovementDate = function (date) {
+  const calcDaysPassed = (date1, date2) => Math.round(Math.abs(date1 - date2) / (1000 * 60 * 60 * 24))
+
+  const daysPassed = calcDaysPassed(new Date(), date)
+
+  if (daysPassed === 0) return 'Today'
+  if (daysPassed === 1) return 'Yesterday'
+  if (daysPassed <= 7) return `${daysPassed}days ago`
+  else {
+    let day = `${date.getDate()}`.padStart(2, 0)
+    let month = `${date.getMonth() + 1}`.padStart(2, 0)
+    let year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  }
+}
+
+const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = '';
 
-  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+  const movs = sort ? acc.movements.slice().sort((a, b) => a - b) : acc.movements;
 
   movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const date = new Date(acc.movementsDates[i])
 
+    const displayDate = formatMovementDate(date)
     const html = `
       <div class="movements__row">
         <div class="movements__type movements__type--${type}">${i + 1
-      } ${type}</div>
+      } ${type}
+        </div>
+        <div class="movements__date">${displayDate}</div>
         <div class="movements__value">${mov.toFixed(0)}€</div>
       </div>
     `;
@@ -141,7 +161,7 @@ createUsernames(accounts);
 
 const updateUI = function (acc) {
   // Display movements
-  displayMovements(acc.movements);
+  displayMovements(acc);
 
   // Display balance
   calcDisplayBalance(acc);
@@ -195,6 +215,9 @@ btnTransfer.addEventListener('click', function (e) {
     // Doing the transfer
     currentAccount.movements.push(-amount);
     receiverAcc.movements.push(amount);
+    currentAccount.movementsDates.push(new Date().toISOString());
+    receiverAcc.movementsDates.push(new Date().toISOString());
+
 
     // Update UI
     updateUI(currentAccount);
@@ -209,6 +232,7 @@ btnLoan.addEventListener('click', function (e) {
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
     currentAccount.movements.push(amount);
+    currentAccount.movementsDates.push(new Date().toISOString());
 
     // Update UI
     updateUI(currentAccount);
@@ -242,73 +266,82 @@ btnClose.addEventListener('click', function (e) {
 let sorted = false;
 btnSort.addEventListener('click', function (e) {
   e.preventDefault();
-  displayMovements(currentAccount.movements, !sorted);
+  displayMovements(currentAccount, !sorted);
   sorted = !sorted;
 });
+
+// DATES
+let presentDay = new Date()
+let day = `${presentDay.getDate()}`.padStart(2, 0)
+let month = `${presentDay.getMonth() + 1}`.padStart(2, 0)
+let year = presentDay.getFullYear();
+let hrs = `${presentDay.getHours()}`.padStart(2, 0);
+let mins = presentDay.getMinutes();
+labelDate.textContent = `${day}/${month}/${year},${hrs}:${mins}`;
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
 
 // CONVERSION
-console.log(Number('234'))
-console.log(+'234')
-console.log(+'234X')
+// console.log(Number('234'))
+// console.log(+'234')
+// console.log(+'234X')
 
 // PARSING 
-console.log(Number.parseInt('23.89'))
-console.log(Number.parseInt('23.89px'))//23
-console.log(Number.parseInt('e23')) //Nan
+// console.log(Number.parseInt('23.89'))
+// console.log(Number.parseInt('23.89px'))//23
+// console.log(Number.parseInt('e23')) //Nan
 
-console.log(Number.parseFloat('e23'))//Nan
-console.log(Number.parseFloat('234'))//234
-console.log(Number.parseFloat('2.45rem'))//2.45
+// console.log(Number.parseFloat('e23'))//Nan
+// console.log(Number.parseFloat('234'))//234
+// console.log(Number.parseFloat('2.45rem'))//2.45
 
-console.log(Number.isNaN(20))//false
-console.log(Number.isNaN('20'))//false '20' ye string hai isko convertnhi krta yeh
-console.log(Number.isNaN(+'20'))//false 
-console.log(Number.isNaN(20 / 0))//false 
+// console.log(Number.isNaN(20))//false
+// console.log(Number.isNaN('20'))//false '20' ye string hai isko convertnhi krta yeh
+// console.log(Number.isNaN(+'20'))//false 
+// console.log(Number.isNaN(20 / 0))//false 
 
 // Checking if value is number
-console.log(Number.isFinite(20))//true
-console.log(Number.isFinite('20'))//false
-console.log(Number.isFinite(+'20X'))//false
-console.log(Number.isFinite(20 / 0))//false
+// console.log(Number.isFinite(20))//true
+// console.log(Number.isFinite('20'))//false
+// console.log(Number.isFinite(+'20X'))//false
+// console.log(Number.isFinite(20 / 0))//false
 // Number.isInteger()
 
 
 // ------MATH FUNCTIONS --------------
-console.log(Math.sqrt(25))//5
-console.log(121 ** (1 / 2))//11
-console.log(121 ** (1 / 3))//4.9437275...
+// console.log(Math.sqrt(25))//5
+// console.log(121 ** (1 / 2))//11
+// console.log(121 ** (1 / 3))//4.9437275...
 
 // MAX()
-console.log(Math.max(1, 2, 54, 21, 34, 234, 7, 876, '90'))//does type coericion but not type parsing
-console.log(Math.max(1, 2, 54, 21, 34, 234, 7, 876, '90x'))//does type coericion but not type parsing --> NaN as '90px' was'nt converted
+// console.log(Math.max(1, 2, 54, 21, 34, 234, 7, 876, '90'))//does type coericion but not type parsing
+// console.log(Math.max(1, 2, 54, 21, 34, 234, 7, 876, '90x'))//does type coericion but not type parsing --> NaN as '90px' was'nt converted
 
 // MIN()
-console.log(Math.min(1, 2, 54, 21, 34, 234, 7, 876, '90'))//does type coericion but not type parsing
+// console.log(Math.min(1, 2, 54, 21, 34, 234, 7, 876, '90'))//does type coericion but not type parsing
 
 // TRUNC()
 
-console.log(Math.trunc(23.9999)) //Returns the integral part of the a numeric expression, x, removing any fractional digits. If x is already an integer, the result is x.
-console.log(Math.trunc(-23.9999))
+// console.log(Math.trunc(23.9999)) //Returns the integral part of the a numeric expression, x, removing any fractional digits. If x is already an integer, the result is x.
+// console.log(Math.trunc(-23.9999))
 
 // ROUNDING INTEGERS
-console.log(Math.round(23.4))//23
-console.log(Math.round(23.5))//24
-console.log(Math.round(23.6))//24
-console.log(Math.round(-23.6))//24  if >=0.5 then n+1 else n
-console.log(Math.round(-23.4))//23
+// console.log(Math.round(23.4))//23
+// console.log(Math.round(23.5))//24
+// console.log(Math.round(23.6))//24
+// console.log(Math.round(-23.6))//24  if >=0.5 then n+1 else n
+// console.log(Math.round(-23.4))//23
 
-console.log(Math.ceil(23.4))//24
-console.log(Math.ceil(23.5))//24  Next greatest integer
-console.log(Math.ceil(23.6))//24 
-console.log(Math.ceil(-23.6))//-23 
+// console.log(Math.ceil(23.4))//24
+// console.log(Math.ceil(23.5))//24  Next greatest integer
+// console.log(Math.ceil(23.6))//24 
+// console.log(Math.ceil(-23.6))//-23 
 
-console.log(Math.floor(23.3))//23 22 21 -- 23
-console.log(Math.floor(23.9))//Returns the greatest integer less than or equal to its numeric argument.
-console.log(Math.floor(-23.9)) //-24 -25 -26 ... so -24
+// console.log(Math.floor(23.3))//23 22 21 -- 23
+// console.log(Math.floor(23.9))//Returns the greatest integer less than or equal to its numeric argument.
+// console.log(Math.floor(-23.9)) //-24 -25 -26 ... so -24
 
 //  floor is better than trun method in handling neg values
 
@@ -317,33 +350,71 @@ console.log(Math.floor(-23.9)) //-24 -25 -26 ... so -24
 const randomNum = (min, max) =>
   Math.floor(Math.random() * ((max - min) + 1) + min)
 
-console.log(randomNum(23, 56))
+// console.log(randomNum(23, 56))
 
 // NUMBER SEPARATOR
 
 const diameter1 = 287460000000;//quite unreadable
 const diameter2 = 287_460_000_000;//quite unreadable
-console.log(diameter1, diameter2);
+// console.log(diameter1, diameter2);
 
 
 const price = 345_99; //$345.99 cents
 
 // cannot do 3._141 or 3_.123 or _3.124 or 3.134_
-console.log(Number(23_40))//2340
-console.log(Number('23_40'))//NaN
-console.log(parseInt('2_3_456'))//2 takes rest as decimal point
+// console.log(Number(23_40))//2340
+// console.log(Number('23_40'))//NaN
+// console.log(parseInt('2_3_456'))//2 takes rest as decimal point
 
 // Big Int
-console.log(123456789876543212345678987654321n)
-console.log(BigInt(2345432))//auto adds n to end of string
-console.log(10000000n + 200000000n)
+// console.log(123456789876543212345678987654321n)
+// console.log(BigInt(2345432))//auto adds n to end of string
+// console.log(10000000n + 200000000n)
 // cannot add bigInt and other dts
 
-console.log(20n>10)//true
-console.log(20n==20)//true
-console.log(20n===20)//false as dt is not same
-console.log(20n=='20')//true
-console.log(123456789098765n+' is huge')//removes n
+// console.log(20n>10)//true
+// console.log(20n==20)//true
+// console.log(20n===20)//false as dt is not same
+// console.log(20n=='20')//true
+// console.log(123456789098765n+' is huge')//removes n
 
-let a1=[0,0,0,0,0,0];
-console.log(a1.fill(1))
+let a1 = [0, 0, 0, 0, 0, 0];
+// console.log(a1.fill(1))
+
+// DATES
+
+const date = new Date();
+console.log(date)
+console.log(new Date('january 13 ,2022'))
+
+account2.movementsDates.forEach(date => {
+  // console.log(new Date(date))
+})
+
+console.log(new Date(2023, 1, 13, 22, 30, 45))
+//year,month,day,hour,min,sec
+// jan 1 1970 is js unix time
+
+console.log(new Date()) //current time - day
+console.log(new Date(0)) // unix time
+console.log(new Date(3 * 24 * 60 * 60 * 1000))//n days from unix time here n=3
+
+const bday = new Date(2023, 2, 30, 17, 30, 10)
+console.log(bday.getFullYear());
+console.log(bday.getMonth());
+console.log(bday.getDate());
+console.log(bday.getHours());
+console.log(bday.getMinutes());
+console.log(bday.getSeconds());
+console.log(bday.getTime())//millis. passed from unix time
+console.log(bday.toISOString());//formatted string
+
+console.log(new Date(bday.getTime()))
+console.log(new Date(Date.now()))
+
+console.log(Date.now());//to get millis. passed from unix time to now
+console.log(new Date())//aj ki puri date wagara sab kuch
+
+const exampleDate = new Date(account1.movementsDates[0])
+
+console.log(exampleDate.getFullYear());
