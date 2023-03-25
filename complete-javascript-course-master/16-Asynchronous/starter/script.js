68abe651cdb74ae25c4459589dcc0b54bbe5391e
Sarthak30-03-2003 <sarthak8544@gmail.com>
2023-03-26 00:28:16 +0530

    thencatch

--- complete-javascript-course-master/16-Asynchronous/starter/script.js
+++ complete-javascript-course-master/16-Asynchronous/starter/script.js
@@ -4,3 +4,36 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+
+const renderCountry = function (data, className = '') {
+    const html = `
+    <article class="country ${className}">
+      <img class="country__img" src="${data.flags.png}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(
+            +data.population / 1000000
+        ).toFixed(1)} people</p>
+        // <p class="country__row"><span>🗣️</span>${data.languages.name}</p>
+        // <p class="country__row"><span>💰</span>${data.currencies.name}</p>
+      </div>
+    </article>
+    `;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+};
+
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(function (response) {
+      console.log(response);
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data[0]);
+      renderCountry(data[0]);
+    });
+};
+
+getCountryData('austria')
