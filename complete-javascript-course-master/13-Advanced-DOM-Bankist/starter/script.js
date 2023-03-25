05d9212e8edad1ba2e8caaa46599f7a7c9471f1d
Sarthak30-03-2003 <sarthak8544@gmail.com>
2023-03-25 23:37:21 +0530

    asyncawait

--- complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
@@ -49,14 +49,14 @@ document.addEventListener('keydown', function (e) {
 
 const header = document.querySelector('.header') //alreadyDone
 const allSection = document.querySelectorAll('.section')
-console.log(allSection) //Returns nodeList
+// console.log(allSection) //Returns nodeList
 
 document.getElementById('section--1');
 const allButtons = document.getElementsByTagName('button')//htmlCollection
-console.log(typeof allButtons) //Returns HTMLCollection 
+// console.log(typeof allButtons) //Returns HTMLCollection 
 // HTML collection changes if we delete add particular elements but Nodelist remains same
 
-console.log(document.getElementsByClassName('btn'))//htmlCollections
+// console.log(document.getElementsByClassName('btn'))//htmlCollections
 
 // CREATING AND INSERTING ELEMENTS
 const message = document.createElement('div'); //creates a div element
@@ -95,15 +95,15 @@ document.querySelector('.btn--close-cookie').addEventListener('click', function 
 // STYLES
 message.style.backgroundColor = '#37383d';
 message.style.width = '120%';
-console.log(message.style.backgroundColor);
-console.log(message.style.width);//these properties are being set by us so we can log or assign them but rest all properties already defined are not accessible easily
+// console.log(message.style.backgroundColor);
+// console.log(message.style.width);//these properties are being set by us so we can log or assign them but rest all properties already defined are not accessible easily
 const messageH = message.style.height
-console.log(message.style.height, messageH);//we get black nothing
+// console.log(message.style.height, messageH);//we get black nothing
 
 // to get those we use computedStyle() 
 // console.log(getComputedStyle(message)) //returns all the properties set on the element by document including set by us , in css ,in js
 // we can use above to get those properties and change or assign them
-console.log(getComputedStyle(message).color)
+// console.log(getComputedStyle(message).color)
 
 const heightMessage = getComputedStyle(message).height //results in string like 12.45px 12rem 23em ...
 message.style.height = Number.parseFloat(heightMessage) + 40 + 'px'
@@ -114,7 +114,7 @@ document.documentElement.style.setProperty('--color-primary', 'orange')
 
 // ATTRIBUTES
 const logo = document.querySelector('.nav__logo');
-console.log(logo.src, logo.alt, logo.class, logo.id)
+// console.log(logo.src, logo.alt, logo.class, logo.id)
 
 // logo.src = ''//gives absolute http::127.0.0...... not /images/ffg
 logo.alt = 'Logo'
@@ -124,9 +124,9 @@ logo.alt = 'Logo'
 
 // setAttribute()  name,      value
 logo.setAttribute('company', 'Bankist')
-console.log(logo.company)//still remains undefined but actually is added
-console.log(logo.getAttribute('company')) //Bankist
-console.log(logo.getAttribute('src')) //give relative path as per our pc
+// console.log(logo.company)//still remains undefined but actually is added
+// console.log(logo.getAttribute('company')) //Bankist
+// console.log(logo.getAttribute('src')) //give relative path as per our pc
 
 // DATA ATTRIBUTES
 /*
@@ -250,5 +250,3 @@ console.log(h1.nextSibling)
 // to get all siblings we select parent then its children
 console.log(h1.parentElement.children)
 console.log([...h1.parentElement.children])
-// 'l-;lp
-// ty
